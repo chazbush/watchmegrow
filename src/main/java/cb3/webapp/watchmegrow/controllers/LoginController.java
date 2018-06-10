32edace743ba31fd35e28b043c9b0bdcb5050720
cb3 <chazbush@gmail.com>
2018-06-10 00:31:50 -0700

    update

--- src/main/java/cb3/webapp/watchmegrow/controllers/LoginController.java
+++ src/main/java/cb3/webapp/watchmegrow/controllers/LoginController.java
@@ -4,15 +4,25 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+
+import cb3.webapp.watchmegrow.services.LoginService;
 
 @Controller
 public class LoginController {
 	
+	LoginService loginservice = new LoginService();
+	
+	@GetMapping({"/login", ""})
+	public String login () {
+		return "login";
+	}
+	
 	@PostMapping("/login")
-	public String login (@RequestParam(name="name") String name, Model model) {
-    	if (name == "kensley") {
+	public String login (@RequestParam String name, Model model) {
+    	if (loginservice.validateUser(name)) {
+    		model.addAttribute("name", name);
     		return "index";
     	}
     	 		
@@ -24,3 +34,4 @@ public class LoginController {
 	}
 }
 
+	
