4ef9cac1155ac4cdd414058b3eeb955f6f6a2949
cb3 <chazbush@gmail.com>
2018-06-09 19:37:32 -0700

    update

--- src/main/java/cb3/webapp/watchmegrow/controllers/IndexController.java
+++ src/main/java/cb3/webapp/watchmegrow/controllers/IndexController.java
@@ -3,12 +3,14 @@ package cb3.webapp.watchmegrow.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+
 @Controller
 public class IndexController {
-	
+			
 	@GetMapping("/")
-	public String login() {
+	public String login () {
 		return "login";
 	}
-
+	
+	
 }
--- src/main/java/cb3/webapp/watchmegrow/controllers/LoginController.java
+++ src/main/java/cb3/webapp/watchmegrow/controllers/LoginController.java
@@ -2,16 +2,25 @@ package cb3.webapp.watchmegrow.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
 	
-	@RequestMapping ("/index")
-	public String login (@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-		model.addAttribute("name", name);
-		return "index";
+	@PostMapping("/login")
+	public String login (@RequestParam(name="name") String name, Model model) {
+    	if (name == "kensley") {
+    		return "index";
+    	}
+    	 		
+    	else {
+    		model.addAttribute("error", "Invalid Name");
+    		return "login";
+    		
+    	}	
 	}
-
 }
+
